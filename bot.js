2786e25cca07ed21e94dc4309bf338e9d28d524b
StaZarycki <stazarycki@gmail.com>
2021-07-11 22:54:29 +0200

    comments are kul

--- bot.js
+++ bot.js
@@ -2,17 +2,21 @@ require("dotenv").config();
 const { Client } = require("discord.js");
 
 const client = new Client();
-const prefix = process.env.PREFIX;
 
+// Initialize server
 client.once("ready", () => {
     console.log("Initializing...");
 
     console.log("Ready!");
 });
 
+// Get messages
 client.on("message", (message) => {
     
     if (message[0] != process.env.PREFIX) return;
+
+
 });
 
+// Login to discord
 client.login(process.env.DISCORD_TOKEN);
